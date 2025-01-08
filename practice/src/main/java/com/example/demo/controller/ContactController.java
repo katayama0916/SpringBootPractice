92e7721bb08a4926995becc09fa4916e557c9fd7
katayama0916 <t.katayama.cingroup2012@gmail.com>
2025-01-09 00:14:03 +0900

    contactform2

--- practice/src/main/java/com/example/demo/controller/ContactController.java
+++ practice/src/main/java/com/example/demo/controller/ContactController.java
@@ -1,35 +1,29 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.data.ContactData;
 
 @Controller
 public class ContactController {
 	@PostMapping("/contact")
-	public ModelAndView contact(@RequestParam("lastName") String lastName,
-			@RequestParam("firstName") String firstName,
-			@RequestParam("email") String email,
-			@RequestParam("phone") String phone,
-			@RequestParam("zipCode") String zipCode,
-			@RequestParam("address") String address,
-			@RequestParam("buildingName") String buildingName,
-			@RequestParam("contactType") String contactType,
-			@RequestParam("body") String body,
-			ModelAndView mv) {
+	public ModelAndView contact(@ModelAttribute ContactData contactdata ,ModelAndView mv)
+			 {
 
 		mv.setViewName("confirmation");
 
-		mv.addObject("lastName", lastName);
-		mv.addObject("firstName", firstName);
-		mv.addObject("email", email);
-		mv.addObject("phone", phone);
-		mv.addObject("zipCode", zipCode);
-		mv.addObject("address", address);
-		mv.addObject("buildingName", buildingName);
-		mv.addObject("contactType", contactType);
-		mv.addObject("body", body);
+		mv.addObject("lastName",   contactdata.getLastName());
+		mv.addObject("firstName", contactdata.getFirstName());
+		mv.addObject("email", contactdata.getEmail());
+		mv.addObject("phone", contactdata.getPhone());
+		mv.addObject("zipCode",  contactdata.getZipCode());
+		mv.addObject("address", contactdata.getAddress());
+		mv.addObject("buildingName", contactdata .getBuildingName());
+		mv.addObject("contactType", contactdata .getContactType());
+		mv.addObject("body", contactdata.getBody());
 
 		return mv;
 	}
